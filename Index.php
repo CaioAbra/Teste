688c821f379251f73cbe19d9a5c1c19ce00f1736
CAIO ABRA <59180237+CaioAbra@users.noreply.github.com>
2019-12-24 00:35:03 -0300

    ajustes de pastas

--- Index.php
+++ Index.php
@@ -1,19 +1,46 @@
-<?php 
-    include 'includes/top.inc.php'; 
-    // require 'services/conect_from_DB.serv.php';
-?> 
+<!DOCTYPE html>
+<html>
 
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel='stylesheet' type='text/css' media='screen' href='css/index.css'>
+    
+    <link rel='stylesheet' type='text/css' media='screen' href='lib/bootstrap-4.3.1-dist/css/bootstrap-grid.min.css'>
+    <link rel='stylesheet' type='text/css' media='screen' href='lib/bootstrap-4.3.1-dist/css/bootstrap-reboot.min.css'>
+    <link rel='stylesheet' type='text/css' media='screen' href='lib/bootstrap-4.3.1-dist/css/bootstrap.min.css'>
 
+    <link rel='stylesheet' type='text/css' media='screen' href='lib/fontawesome-5.8.2-web/css/all.min.css'>
+    <link rel='stylesheet' type='text/css' media='screen' href='lib/fontawesome-5.8.2-web/css/brands.min.css'>
+    <link rel='stylesheet' type='text/css' media='screen' href='lib/fontawesome-5.8.2-web/css/fontawesome.min.css'>
+    <link rel='stylesheet' type='text/css' media='screen' href='lib/fontawesome-5.8.2-web/css/regular.min.css'>
+    <link rel='stylesheet' type='text/css' media='screen' href='lib/fontawesome-5.8.2-web/css/solid.min.css'>
+    <link rel='stylesheet' type='text/css' media='screen' href='lib/fontawesome-5.8.2-web/css/svg-with-js.min.css'>
+    <link rel='stylesheet' type='text/css' media='screen' href='lib/fontawesome-5.8.2-web/css/v4-shims.min.css'>
+    <title>Teste</title>
+</head>
 
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-3">
+                oi
+            </div>
 
-<!--------------------------------------------------------------- corpo --------------------------------------------------------------->
-<div class="container">
-    <div class="row mt-3 ">
-        <h3>testando os includes</h3>
+            <div class="col-sm-9 bg-grey">
+                <h1 class="pink">oi</h1>
+            </div>
+
+        </div>
     </div>
-<div>
-<!------------------------------------------------------------- fim corpo ------------------------------------------------------------->
+</body>
 
-<?php
-   include 'includes/botoom.inc.php';
-?> 
+<script src='js/index.js'></script>
+<script src='js/jquery/jquery-3.3.1.min.js'></script>
+<script src='js/jquery/jquery-ui.min.js'></script>
+
+<script src='lib/bootstrap-4.3.1-dist/js/bootstrap.bundle.min.js'></script>
+<script src='lib/bootstrap-4.3.1-dist/js/bootstrap.min.js'></script>
+
+</html>
--- includes/botoom.inc.php
+++
@@ -1,9 +0,0 @@
-    </body>
-    <script src='js/index.js'></script>
-    <script src='js/jquery/jquery-3.3.1.min.js'></script>
-    <script src='js/jquery/jquery-ui.min.js'></script>
-    <!------------------ bootstrap ------------------>
-    <script src='lib/bootstrap-4.3.1-dist/js/bootstrap.bundle.min.js'></script>
-    <script src='lib/bootstrap-4.3.1-dist/js/bootstrap.min.js'></script>
-    <!---------------- fim bootstrap ---------------->
-</html>
