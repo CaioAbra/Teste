caf936ddc5c8181c0284ffc6ad39e319d9836663
CAIO ABRA <59180237+CaioAbra@users.noreply.github.com>
2019-12-23 21:31:21 -0300

    first commit
    primeiro comit de teste

---
+++ Index.php
@@ -0,0 +1,19 @@
+<?php 
+    include 'includes/top.inc.php'; 
+    // require 'services/conect_from_DB.serv.php';
+?> 
+
+
+
+
+<!--------------------------------------------------------------- corpo --------------------------------------------------------------->
+<div class="container">
+    <div class="row mt-3 ">
+        <h3>testando os includes</h3>
+    </div>
+<div>
+<!------------------------------------------------------------- fim corpo ------------------------------------------------------------->
+
+<?php
+   include 'includes/botoom.inc.php';
+?> 
