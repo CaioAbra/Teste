4516e2b64e9ee87aa481160bffd56d5b55790a67
CAIO ABRA <59180237+CaioAbra@users.noreply.github.com>
2019-12-24 03:14:29 -0300

    atualização 01

--- Index.php
+++ Index.php
@@ -21,16 +21,153 @@
     <title>Teste</title>
 </head>
 
-<body>
+<body class="bg-light">
     <div class="container-fluid">
-        <div class="row">
-            <div class="col-sm-3">
-                oi
+        <div class="row mt-3">
+            <div class="col-sm-2 bg-white">
+                <div class="logo border border-dark">
+                    <h3 class="p-3 text-center text-dark">Logo</h3>
+                </div>
+                <ul class="nav flex-column text-sm-center text-md-left">
+                    <li class="nav-item d-inline">
+                        <a class="nav-link" href="#"><i class="fas fa-cubes pr-3"></i>>Opção 1</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#"><i class="far fa-file  pr-3"></i>Opção 2</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#"><i class="far fa-star  pr-3"></i>Opção 3</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#"><i class="far fa-id-badge  pr-3"></i>Opção 4</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#"><i class="fas fa-shield-alt  pr-3"></i>Opção 5</a>
+                    </li>
+                </ul>
+                <!-- <div class="fixed-bottom">...</div> -->
             </div>
 
-            <div class="col-sm-9 bg-grey">
-                <h1 class="pink">oi</h1>
+            <div class="col-sm-10 ">
+                <h3 class="pb-2 border-bottom">Título da página</h3>
+
+                <div class="col-12 mt-4 border rounded bg-caixa shadow-box-princ">
+                    <form class="form-inline">
+                        <div class="col-12 pt-3 pl-2">
+                            <h5>Subtitulo</h5>
+                        </div>
+
+                        <div class="row">
+                            <div class="col-12  col-lg-4">
+                                <div class="form-group">
+                                    <label for="input1">Textarea Label</label>
+                                    <input type="text" class="form-control" id="input1" placeholder="Placeholder Text">
+                                </div>
+                            </div>
+
+                            <div class="col-12  col-lg-4">
+                                <div class="form-group">
+                                    <label for="input2">Example select</label>
+                                    <select class="form-control" id="input2">
+                                        <option selected class="muted">Selecione...</option>
+                                        <option>2</option>
+                                        <option>3</option>
+                                        <option>4</option>
+                                        <option>5</option>
+                                    </select>
+                                </div>
+                            </div>
+
+                            <div class="col-12  col-lg-4">
+                                <div class="form-group">
+                                    <label for="input3">Textarea Label</label>
+                                    <input type="text" class="form-control" id="input3" placeholder="Placeholder Text">
+                                </div>
+                            </div>
+                        </div>
+
+
+
+                        <div class="row mt-lg-4">
+                            <div class="col-12  col-lg-4">
+                                <div class="form-group">
+                                    <label for="input4">Textarea Label</label>
+                                    <input class="form-control" type="text" placeholder="Placeholder Text" readonly>
+                                </div>
+                            </div>
+
+                            <div class="col-12  col-lg-4">
+                                <div class="form-group">
+                                    <label for="input5">Textarea Label</label>
+                                    <input type="text" class="form-control" id="input5" placeholder="Placeholder Text">
+                                </div>
+                            </div>
+
+                            <div class="col-12  col-lg-4">
+                                <div class="form-group">
+                                    <label for="input6">Textarea Label <span class="ml-5 pl-3">(opcional)</span></label>
+                                    <input type="text" class="form-control " id="input6" placeholder="Placeholder Text">
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-12 pt-3 pl-2 mt-3">
+                            <h5>Subtitulo</h5>
+                        </div>
+
+                        <div class="row">
+                            <div class="col-12  col-lg-4">
+                                <div class="form-group">
+                                    <label for="input7">Textarea select <span class="ml-5 pl-3">(opcional)</span></label>
+                                    <select class="form-control" id="input7">
+                                        <option selected class="muted">Selecione...</option>
+                                        <option>2</option>
+                                        <option>3</option>
+                                        <option>4</option>
+                                        <option>5</option>
+                                    </select>
+                                </div>
+                            </div>
+
+                            <div class="col-12  col-lg-4">
+                                <div class="form-group">
+                                    <label for="input8">Example select</label>
+                                    <select class="form-control" id="input8">
+                                        <option selected class="muted">Selecione...</option>
+                                        <option>2</option>
+                                        <option>3</option>
+                                        <option>4</option>
+                                        <option>5</option>
+                                    </select>
+                                </div>
+                            </div>
+
+                            <div class="col-12  col-lg-4">
+                                <div class="form-group">
+                                    <label for="input9">Textarea Label</label>
+                                    <select class="form-control" id="input9">
+                                        <option selected class="muted">Selecione...</option>
+                                        <option>2</option>
+                                        <option>3</option>
+                                        <option>4</option>
+                                        <option>5</option>
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="row">
+                           <div class="col-12  col-lg-4">
+                                <a class="btn btn-green" href="#" role="button">Link</a>
+                           </div>
+                        </div>
+
+                    </form>
+
+                </div>
             </div>
+
+
 
         </div>
     </div>
